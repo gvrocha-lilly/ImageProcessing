5f820d8fd917c40d0d1fdd72385cf05916db6824
Tooba Abbassi-Daloii <31986271+tabbassidaloii@users.noreply.github.com>
2022-11-24 09:50:51 +0100

    Update step1_Myofiber_Analysis_Pipeline.bat

--- MyofiberTyping/Macros/BatchFiles/step1_Myofiber_Analysis_Pipeline.bat
+++ MyofiberTyping/Macros/BatchFiles/step1_Myofiber_Analysis_Pipeline.bat
@@ -2,10 +2,10 @@
 setlocal EnableDelayedExpansion
 
 :: Note: replace the path below with complete path to CZI datasets
-SET datapath=I:\tabbassidaloii\ImageProcessing\MyofiberTyping\Samples\ 
+SET datapath=I:\tabbassidaloii\ImageProcessing\MyofiberTyping\Samples\
 
 :: Note: replace the path below with complete path to macro files
-SET macropath=I:\\tabbassidaloii\ImageProcessing\MyofiberTyping\Macros\ 
+SET macropath=I:\\tabbassidaloii\ImageProcessing\MyofiberTyping\Macros\
 
 :: Note: replace the path below with complete path to Fiji app
 SET imagejPath=C:\Fiji.app\ImageJ-win64.exe
